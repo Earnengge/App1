99c2aef64a9f15ed9b2d176428d548ac42f9d671
Godspower Ufot <godspowerufot>
2023-10-27 15:26:13 +0100

    new changes

--- src/component/pages/Profile.jsx
+++ src/component/pages/Profile.jsx
@@ -5,8 +5,8 @@ function Profile() {
     <>
 
 <div className="profile-for-web3">
-    <div className="profile-avater">
-    <img class="img-responsive" loading="lazy" src="https://cdn.simplehash.com/assets/db17eebeede377614b156126590d5e4c521a80fef6bdce78e8e6563b4526b417.gif"/>
+    <div className="profile-avater" style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
+    <img className="img-responsive" loading="lazy" src="https://cdn.simplehash.com/assets/db17eebeede377614b156126590d5e4c521a80fef6bdce78e8e6563b4526b417.gif"/>
     <div className="profile-name">
         <h2>vitalik.eth</h2>
     </div>
