e0cb26292faafaca186c873afda67a3be06cd42c
Godspower Ufot <godspowerufot>
2023-10-27 10:39:13 +0100

    commit messages

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web3-work',
-  server: {
-    // ...
-    // Remove or modify Permissions-Policy as needed
-    headers: {
-      'Permissions-Policy': 'geolocation=(), microphone=(), camera=()',
-    },
-  },
+ 
 })
